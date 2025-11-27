5e15d41ddbf0bc1b25b0a6fd58ef1eed7e98fb93
Yavuz Selim Kaya <yselimkaya0@gmail.com>
2025-11-27 13:00:48 +0300

    Create mergetwosortedlists.c

---
+++ 3_mergetwosortedlists/mergetwosortedlists.c
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
+    struct ListNode sorted;
+    struct ListNode *tail = &sorted;
+
+    while (list1 != NULL && list2 != NULL) {
+        if (list1->val <= list2->val) {
+            tail->next = list1;
+            list1 = list1->next;
+        } else {
+            tail->next = list2;
+            list2 = list2->next;
+        }
+        tail = tail->next;
+    }
+    if (list1 != NULL) {
+        tail->next = list1;
+    } else if (list2 != NULL) {
+        tail->next = list2;
+    }
+    return sorted.next;
+} 
